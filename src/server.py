dd69be6fbb8e763efd27cf9d281a9639a14ef399
ninz <nreclarin@gmail.com>
2023-03-06 23:54:31 +0800

    added dataclass definitions on test api

--- src/server.py
+++ src/server.py
@@ -1,6 +1,35 @@
 from fastapi import FastAPI
-
+# Enhanced dataclass.dataclass object
+from pydantic.dataclasses import dataclass
 app = FastAPI()
+
+
+@dataclass
+class Rider:
+    name: str = ""
+    age: int = 0
+    weight: int = 0
+    height: int = 0
+
+
+@dataclass
+class Suspension:
+    brand: str = ""
+    travel: int = 0
+    model: str = ""
+
+@dataclass
+class Bike:
+    frame_brand: str = ""
+    model: str = ""
+    rear_suspension: Suspension = None
+    front_suspension: Suspension = None
+
+
+@dataclass
+class GetRiderResponse:
+    rider: Rider
+    bike: Bike
 
 
 def generate_data() -> dict:
@@ -34,8 +63,29 @@ def generate_data() -> dict:
         }
 
     }
-    
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/get_rider/json")
+async def get_rider_json():
+    """
+    Json api call.
+
+    :return:
+    :rtype:
+    """
+
+    rider_profile = generate_data()
+
+    return rider_profile
+
+
+@app.get("get_rider/dataclass")
+async def get_rider_dataclass():
+    """
+    Dataclass api call.
+
+    :return:
+    :rtype:
+    """
+
+    return {}
